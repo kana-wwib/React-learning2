9ad83d3fc0bdc7c07ef2f7e450e1ab1b5a5391df
kana-wwib <mukrabwy103@gmail.com>
2020-08-23 19:05:24 +0900

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,106 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';	
+import './assets/styles/style.css'	
 import defaultDataset from "./dataset";
+import {db} from './firebase/index'	
 import './assets/styles/style.css';
+import {AnswersList, Chats, Loading} from './components/index'	
+import {FormDialog} from "./components/Forms/index";
 
-export default class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      answer: [],
-      chats: [],
-      currentId: "init",
-      dataset: defaultDataset,
-      open: false
-    }
+
+const App = () => {
+  const [answers, setAnswers] = useState ([]);
+  const [chats, setChats] = useState ([]);
+  const [currentId, setCurrentId] = useState ("init");
+  const [dataset, setDataset] = useState ({});
+  const [open, setOpen] = useState (false);
+
+  const addChats = (chat) => {
+    setChats(prevChats => {
+      return [...prevChats, chat]
+    })
   }
 
-  render () {
+  const handleClickOpen = () => {
+    setOpen(true)
+  };
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+
+  const displayNextQuestion = (nextQuestionId, nextDataset) => {
+    addChats ({
+      text: nextDataset.question,
+      type: 'question'
+    });
+    
+      setAnswers(nextDataset.answers)
+      setCurrentId(nextQuestionId)
+  }
+
+  const selectAnswer = useCallback((selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'contact'):
+        handleClickOpen();
+        break;
+
+      case (/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href= nextQuestionId;
+        a.target = '_blank'; //別タブで開く
+        a.click();
+        break;
+
+      default:
+        addChats({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+
+      setTimeout(() => displayNextQuestion(nextQuestionId, dataset[nextQuestionId]), 1000)
+        break;
+    }
+  },[answers]);
+
+
+
+
+  useEffect(() => {
+    (async() => {
+      const initDataset = {};
+
+      await db.collection('question').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+
+      setDataset(initDataset)
+      displayNextQuestion(currentId, initDataset[currentId])
+    })()
+  }, [])
+
+  useEffect(() => {
+    const scrollArea = document.getElementById('scroll-area');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
+  })
+
+
     return (
       <section className='c-section'>
         <div className='c-box'>
-          {this.state.currentId}
+          <Chats chats={chats} />
+          <AnswersList answers={answers} select={selectAnswer} />
+          <FormDialog open={open} handleClose={handleClose}/>
         </div>
       </section>
     );
-  }
 }
 
+
+export default App
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpq_CGwO4fJ5zHpW4qaP4z8peRcBBYLa4",
+  authDomain: "chatbot-demo-316ef.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-316ef.firebaseio.com",
+  projectId: "chatbot-demo-316ef",
+  storageBucket: "chatbot-demo-316ef.appspot.com",
+  messagingSenderId: "124202587040",
+  appId: "1:124202587040:web:e083c056a70ece2a51cac5",
+  measurementId: "G-HSD2R9VTMB"
+};
+
+export default firebaseConfig;
